7508ce5ae193daec7df86ff9810dca062a1cb635
Guy <95033937+roncheers@users.noreply.github.com>
2022-02-09 08:55:28 +0200

    created artist class

---
+++ core/__init__.py
@@ -0,0 +1,21 @@
+{
+    "artist_id": {
+        "name": "",
+        "albums": ["id", "id"]
+    }
+}
+
+{
+    "album_id": {
+        "name": "",
+        "songs": []
+    }
+}
+
+
+{
+    "song_id": {
+        "name": "guy",
+        "popular": ""
+    }
+}
