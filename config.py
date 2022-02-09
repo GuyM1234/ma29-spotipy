66b31e1477ef81e5c0f814c6bc41287c5f908ebc
Guy <95033937+roncheers@users.noreply.github.com>
2022-02-09 09:38:56 +0200

    starting add track logic

---
+++ config.py
@@ -0,0 +1,5 @@
+PATHS = {
+    "albums_dir":  r"C:\Users\user\Desktop\Code\ma29-spotipy\db\albums",
+    "songs_dir":  r"C:\Users\user\Desktop\Code\ma29-spotipy\db\songs",
+    "artists_dir":  r"C:\Users\user\Desktop\Code\ma29-spotipy\db\artists",
+}
---
+++ core/models.py
@@ -0,0 +1,33 @@
+from core.storage.readers import json_reader
+from core.storage.writers import json_writer
+from config import PATHS
+
+
+def write_track_to_db(song):
+    pass
+
+
+def add_track(read_from: str, reader=json_reader, writer=write_track_to_db):
+    track = reader(read_from)
+    print(track)
+
+{
+    "artist_id": {
+        "name": "",
+        "albums": ["id", "id"]
+    }
+}
+
+{
+    "album_id": {
+        "name": "",
+        "songs": []
+    }
+}
+
+{
+    "song_id": {
+        "name": "guy",
+        "popular": ""
+    }
+}
