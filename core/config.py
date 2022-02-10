a612448066f913e4c1dac5b773017c05366da166
Guy <95033937+roncheers@users.noreply.github.com>
2022-02-10 15:43:39 +0200

    changed authentication method logic in searching

--- core/config.py
+++ core/config.py
@@ -3,8 +3,8 @@ import logging
 PATHS = {
     "tracks": r"C:\Users\user\Desktop\Code\ma29-spotipy\db\tracks.json",
     "users": r"C:\Users\user\Desktop\Code\ma29-spotipy\db\users.json",
-    "new_songs": r"C:\Users\user\Desktop\Code\ma29-spotipy\songs",
-    "audio_features": r"C:\Users\user\Desktop\Code\ma29-spotipy\audio_features",
+    "new_songs": r"C:\Users\user\Desktop\Code\ma29-spotipy\db\songs",
+    "audio_features": r"C:\Users\user\Desktop\Code\ma29-spotipy\db\audio_features",
     "log_file": r"C:\Users\user\Desktop\Code\ma29-spotipy\db\log.txt",
 }
 FREE = "free"
@@ -12,5 +12,6 @@ PREMIUM = "premium"
 logging.basicConfig(filename=PATHS['log_file'], encoding='utf-8', level=logging.DEBUG)
 MAX_PLAYLISTS_FOR_FREE_ACC = 5
 MAX_PLAYLIST_TRACKS_FOR_FREE_ACC = 20
+MAX_SEARCH_RESULT_FOR_FREE_ACC = 5
 AUDIO_FEATURES = {"danceability", "energy", "acousticness", "instrumentalness", "valence"}
 DEFAULT_PREFERENCES = {audio_feature: 0.5 for audio_feature in AUDIO_FEATURES}
--- core/models/searching.py
+++ core/models/searching.py
@@ -1,57 +1,50 @@
 from operator import itemgetter
+from typing import Iterable
 
-from core.config import PATHS, FREE
+from core.config import PATHS, FREE, MAX_SEARCH_RESULT_FOR_FREE_ACC
 from core.models.exceptions import MethodIsCorrupted
 from core.models.user.utils import get_user
 from core.models.utils import read
 
 
-def authenticate(func):
-    def wrapper(username, *args):
-        result = func(*args)
-        return list(result)[0:5] if get_user(username)['type'] == FREE else result
-
-    return wrapper
+def authenticate(result: Iterable, username: str):
+    return list(result)[0:MAX_SEARCH_RESULT_FOR_FREE_ACC] if get_user(username)['type'] == FREE else result
 
 
-@authenticate
-def get_artists():
+def get_artists(username: str):
     tracks = read(PATHS['tracks'])
     artists = set()
     for track in tracks.values():
         for artist in track['artists']:
             artists.add(artist['name'])
 
-    return artists
+    return authenticate(artists, username)
 
 
-@authenticate
-def get_artist_album(artist_id: str):
+def get_artist_album(username: str, artist_id: str):
     tracks = read(PATHS['tracks'])
-    return {track['album']['name'] for track in tracks.values() if
-            artist_id in [artist['id'] for artist in track['artists']]}
+    albums = {track['album']['name'] for track in tracks.values() if
+              artist_id in [artist['id'] for artist in track['artists']]}
+    return authenticate(albums, username)
 
 
-@authenticate
-def get_best_artist_songs(artist_id: str):
+def get_best_artist_songs(username: str, artist_id: str):
     tracks = read(PATHS['tracks'])
-    songs = {(track['name'], track['popularity']) for track in tracks.values() if
-             artist_id in [artist['id'] for artist in track['artists']]}
-    return sorted(songs, key=itemgetter(1), reverse=True)
+    songs = {(track['name'], track['popularity']) for track in tracks.values()
+             if artist_id in [artist['id'] for artist in track['artists']]}
+    return authenticate(sorted(songs, key=itemgetter(1), reverse=True), username)
 
 
-@authenticate
-def get_album_songs(album_id: str):
+def get_album_songs(username: str, album_id: str):
     tracks = read(PATHS['tracks'])
-    return {track['name'] for track in tracks.values() if track['album']['id'] == album_id}
+    songs = {track['name'] for track in tracks.values() if track['album']['id'] == album_id}
+    return authenticate(songs, username)
 
 
 # template for more searching methods
-@authenticate
 def generic_search_method(func, *args):
     tracks = read(PATHS['tracks'])
     try:
-        return func(tracks, *args)
+        return authenticate(func(tracks, *args))
     except Exception:
         raise MethodIsCorrupted()
-
